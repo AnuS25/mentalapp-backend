90a10e84fa3bd3632aa02271e0593b9d14c8246b
AnuS25 <asailoni8@gmail.com>
2025-01-12 22:24:44 +0530

    Update mood.js

--- mood.js
+++ mood.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const moodSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  mood: { type: String, required: true }, // e.g., "Happy", "Sad", etc.
+  mood: { type: String, required: true }, 
   date: { type: Date, default: Date.now },
 });
 
