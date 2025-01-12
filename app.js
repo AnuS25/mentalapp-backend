f4112c62bc3cc2c205dc3453b46b95dbec47f70d
AnuS25 <asailoni8@gmail.com>
2025-01-12 22:30:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -138,8 +138,9 @@ const verifyToken = (req, res, next) => {
 
 app.post('/moods',verifyToken, async (req, res) => {
   console.log('Route /moods hit');
-   const { mood } = req.body;
-  const userId = req.userId; 
+   //const { mood } = req.body;
+  const userId = req.userId;
+  const mood='Happy';
   try {
     //const { userId, mood } = req.body;
     const newMood = new Mood({ userId, mood });
