eecdfd9790bb4c4f249d5712aca4c88c68d9aeb8
AnuS25 <asailoni8@gmail.com>
2025-01-26 15:00:58 +0530

    Update journal.js

--- journal.js
+++ journal.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const journalSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true, 
+    ref: "userinfo" // Reference to the "userinfo" collection
+  },,
   title: { type: String, required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
