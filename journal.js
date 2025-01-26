aae2668d4a25e10d6fc3a0baaec0ac59d56ee87b
AnuS25 <asailoni8@gmail.com>
2025-01-26 12:51:05 +0530

    Create journal.js

---
+++ journal.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const journalSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Journal", journalSchema);
